000819997672104fdd4e5f6668dc2d4892719be2
Mehak Fatima <139680303+MehakRauf@users.noreply.github.com>
2026-02-28 13:59:47 +0500

    Implement merge sort and quick sort algorithms
    Added merge sort and quick sort implementations to Sorting.cpp.

--- DSA/Arrays/Sorting.cpp
+++ DSA/Arrays/Sorting.cpp
@@ -62,9 +62,85 @@ void insertionSort(vector<int> arr)
     }
     cout << endl;
 }
+
+void merge(vector<int> &arr, int l, int mid, int r)
+{
+    vector<int> temp;
+    int i = l, j = mid + 1;
+    while (i <= mid && j <= r)
+    {
+        if (arr[i] <= arr[j])
+        {
+            temp.push_back(arr[i]);
+            i++;
+        }
+        else
+        {
+            temp.push_back(arr[j]);
+            j++;
+        }
+    }
+
+    while (i <= mid)
+    {
+        temp.push_back(arr[i]);
+        i++;
+    }
+    while (j <= r)
+    {
+        temp.push_back(arr[j]);
+        j++;
+    }
+    for (int i = 0; i < temp.size(); i++)
+    {
+        arr[l + i] = temp[i];
+    }
+}
+
+void mergeSort(vector<int> &arr, int l, int r)
+{
+    if (l < r)
+    {
+        int mid = l + (r - l) / 2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+}
+
+int partition(vector<int> &arr, int low, int high)
+{
+    int i = low - 1, pivot = arr[high];
+    for (int j = low; j <= high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(vector<int> &arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = partition(arr, low, high);
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
+    }
+}
+
 int main()
 {
-    vector<int> arr = {1, 3, 2, 4, 8, 3, 7};
-    insertionSort(arr);
+
+    vector<int> arr = {1, 13, 2, 4, 18, 93, 7};
+    quickSort(arr, 0, arr.size() - 1);
+    for (int val : arr)
+    {
+        cout << val << " ";
+    }
     return 0;
 }
